8048113ca5ad908028adb527f4ef969cb3637abf
SS <majhi9442@gmail.com>
2021-06-24 15:37:48 +0530

    Initial commit

---
+++ app/src/main/java/in/pm/nearapp/Class/GetNearbyPlacesData.java
@@ -0,0 +1,98 @@
+package in.pm.nearapp.Class;
+
+import android.content.Context;
+import android.graphics.Bitmap;
+import android.graphics.Canvas;
+import android.graphics.drawable.Drawable;
+import android.os.AsyncTask;
+import android.util.Log;
+
+import androidx.annotation.DrawableRes;
+import androidx.core.content.ContextCompat;
+
+import com.google.android.gms.maps.CameraUpdateFactory;
+import com.google.android.gms.maps.GoogleMap;
+import com.google.android.gms.maps.model.BitmapDescriptor;
+import com.google.android.gms.maps.model.BitmapDescriptorFactory;
+import com.google.android.gms.maps.model.LatLng;
+import com.google.android.gms.maps.model.MarkerOptions;
+
+import java.io.IOException;
+import java.util.HashMap;
+import java.util.List;
+
+import in.pm.nearapp.R;
+
+/**
+ * @author Prakash
+ */
+
+public class GetNearbyPlacesData extends AsyncTask<Object, String, String> {
+
+    private String googlePlacesData;
+    private GoogleMap mMap;
+    String url;
+    Context context;
+    @Override
+    protected String doInBackground(Object... objects){
+        mMap = (GoogleMap)objects[0];
+        url = (String)objects[1];
+
+        DownloadURL downloadURL = new DownloadURL();
+        try {
+            googlePlacesData = downloadURL.readUrl(url);
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+
+        return googlePlacesData;
+    }
+
+    @Override
+    protected void onPostExecute(String s){
+
+        List<HashMap<String, String>> nearbyPlaceList;
+        DataParser parser = new DataParser();
+        nearbyPlaceList = parser.parse(s);
+        Log.d("nearbyplacesdata","called parse method");
+        showNearbyPlaces(nearbyPlaceList);
+    }
+
+    private void showNearbyPlaces(List<HashMap<String, String>> nearbyPlaceList)
+    {
+        for(int i = 0; i < nearbyPlaceList.size(); i++)
+        {
+            MarkerOptions markerOptions = new MarkerOptions();
+            HashMap<String, String> googlePlace = nearbyPlaceList.get(i);
+
+            String placeName = googlePlace.get("place_name");
+            String vicinity = googlePlace.get("vicinity");
+            double lat = Double.parseDouble( googlePlace.get("lat"));
+            double lng = Double.parseDouble( googlePlace.get("lng"));
+
+            LatLng latLng = new LatLng( lat, lng);
+            markerOptions.position(latLng);
+            markerOptions.title(placeName + " : "+ vicinity);
+            markerOptions.icon(BitmapDescriptorFactory.defaultMarker(BitmapDescriptorFactory.HUE_RED));
+
+            //markerOptions.icon(bitmapDescriptorFromVector(context, R.drawable.ic_info_check));
+
+            mMap.addMarker(markerOptions);
+            mMap.moveCamera(CameraUpdateFactory.newLatLng(latLng));
+            mMap.animateCamera(CameraUpdateFactory.zoomTo(15));
+        }
+
+    }
+
+    private BitmapDescriptor bitmapDescriptorFromVector(Context context, @DrawableRes int vectorDrawableResourceId) {
+        Drawable background = ContextCompat.getDrawable(context, R.drawable.ic_info_check);
+        background.setBounds(0, 0, background.getIntrinsicWidth(), background.getIntrinsicHeight());
+        Drawable vectorDrawable = ContextCompat.getDrawable(context, vectorDrawableResourceId);
+        vectorDrawable.setBounds(40, 20, vectorDrawable.getIntrinsicWidth() + 40, vectorDrawable.getIntrinsicHeight() + 20);
+        Bitmap bitmap = Bitmap.createBitmap(background.getIntrinsicWidth(), background.getIntrinsicHeight(), Bitmap.Config.ARGB_8888);
+        Canvas canvas = new Canvas(bitmap);
+        background.draw(canvas);
+        vectorDrawable.draw(canvas);
+        return BitmapDescriptorFactory.fromBitmap(bitmap);
+    }
+}
---
+++ app/src/main/java/in/pm/nearapp/Spalsh.java
@@ -0,0 +1,89 @@
+package in.pm.nearapp;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.annotation.SuppressLint;
+import android.content.Intent;
+import android.content.pm.PackageManager;
+import android.os.Build;
+import android.os.Bundle;
+import android.os.Handler;
+import android.view.Window;
+import android.view.WindowManager;
+import android.widget.TextView;
+
+import com.google.firebase.auth.FirebaseAuth;
+
+import in.pm.nearapp.Activity.NearBy;
+import in.pm.nearapp.Activity.SignIn;
+
+public class Spalsh extends AppCompatActivity {
+    FirebaseAuth firebaseAuth;
+    @SuppressLint("SetTextI18n")
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        firebaseAuth = FirebaseAuth.getInstance();
+        TextView textView= findViewById(R.id.tv_splash_app_title);
+        TextView textView1= findViewById(R.id.tv_splash_app_version);
+
+        try{
+            String versionName = this.getPackageManager()
+                    .getPackageInfo(this.getPackageName(), 0).versionName;
+            textView1.setText("V "+ versionName);
+        } catch (PackageManager.NameNotFoundException e) {
+            e.printStackTrace();
+            return;
+        }
+
+
+
+
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.KITKAT) {
+            Window w = getWindow();
+            w.setFlags(WindowManager.LayoutParams.FLAG_LAYOUT_NO_LIMITS, WindowManager.LayoutParams.FLAG_LAYOUT_NO_LIMITS);
+        }
+
+        final Handler handler = new Handler();
+        Runnable runnable = new Runnable() {
+
+            int count = 0;
+
+            @Override
+            public void run() {
+                count++;
+
+                if (count == 1)
+                {
+                    textView.setText("Loading.");
+                }
+                else if (count == 2)
+                {
+                    textView.setText("Loading..");
+                }
+                else if (count == 3)
+                {
+                    textView.setText("Loading...");
+                }
+
+                if (count == 3)
+                    Load();
+
+                handler.postDelayed(this, 2 * 1000);
+            }
+        };
+        handler.postDelayed(runnable, 1 * 1000);
+
+    }
+
+    public void Load(){
+        if (firebaseAuth.getCurrentUser() != null) {
+            startActivity(new Intent(Spalsh.this, NearBy.class));
+            Spalsh.this.finish();
+        } else {
+            startActivity(new Intent(Spalsh.this, SignIn.class));
+        }
+    }
+}
